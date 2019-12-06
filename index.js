88986d50295653a90648ac3b06e21d13ff165506
spencerwright1993 <spencer.wright1993@gmail.com>
2019-12-05 18:42:37 -0700

    Updated for heroku

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const uuidv4 = require('uuid/v4');
-const fs = require('fs');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
